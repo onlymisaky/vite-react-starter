985820770635fad8d3ae0a0e8c07b4a81252cc15
mikasa <onlymisaky@gmail.com>
2025-02-07 19:11:56 +0800

    feat: 添加 css 预处理器和后处理器

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+/**
+ * @link https://github.com/postcss/postcss#usage
+ * @link https://github.com/postcss/postcss-load-config/
+ * @type {import('postcss-load-config').Config}
+ */
+export default {
+  plugins: {
+    /**
+     * @link https://github.com/postcss/postcss-nested
+     * @description 如果仅需嵌套、变量语法，使用 PostCss 性能比 scss 更优
+     */
+    'postcss-nested': {},
+    autoprefixer: {},
+  },
+};
