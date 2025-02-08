0c4fda53f2cd3f05cd995c09c54bd0bbb3e7ae04
mikasa <onlymisaky@gmail.com>
2025-02-08 11:13:31 +0800

    feat: 添加 code-inspector 插件以增强代码审查

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import * as path from 'node:path';
 import react from '@vitejs/plugin-react-swc';
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig((config) => {
   const envDir = path.resolve(process.cwd(), './environments');
   const env = loadEnv(config.mode, envDir) as unknown as ViteEnv;
-  console.log(env);
 
   return {
     base: process.env.BASE_URL || env.BASE_URL || '/',
     envDir,
-    plugins: [react()],
+    plugins: [
+      react(),
+      codeInspectorPlugin({
+        bundler: 'vite',
+      }),
+    ],
     css: {
       preprocessorOptions: {
         scss: {
